07753ed5771abdf6befa490f5f87a27ed30ffa8d
DeniSusanto <DeniSusanto@users.noreply.github.com>
2018-11-07 10:35:29 +0800

    add  searchbar (currently commented out in cm_home)

--- ASP/main/admin.py
+++ ASP/main/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
 from .models import *
 from django.contrib.auth.models import Group, User
-# Register your models here.
 
-admin.site.register(ItemCategory)
-admin.site.register(ItemCatalogue)
 admin.site.register(ClinicManager)
-admin.site.register(Clinic)
-admin.site.register(WarehousePersonnel)
 admin.site.register(Dispatcher)
-admin.site.register(HospitalAuthority)
-admin.site.register(Cart)
 admin.site.register(ItemsInCart)
 admin.site.register(ItemsInOrder)
 admin.site.register(Order)
 admin.site.register(OrderRecord)
+
+# #temporarily ommited from admin panel
+# admin.site.register(ItemCatalogue)
+# admin.site.register(ItemCategory)
+# admin.site.register(Clinic)
+# admin.site.register(WarehousePersonnel)
+# admin.site.register(HospitalAuthority)
+# admin.site.register(Cart)
 
 #unregister
 admin.site.unregister(User)
