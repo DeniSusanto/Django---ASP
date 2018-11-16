e149a9a0c1dc5a3f3d64c5b4a83874513e46c989
marvinali98 <44761783+marvinali98@users.noreply.github.com>
2018-11-16 23:04:30 +0800

    token in admin

--- ASP/main/admin.py
+++ ASP/main/admin.py
@@ -17,6 +17,24 @@ admin.site.register(Clinic)
 # admin.site.register(HospitalAuthority)
 admin.site.register(Cart)
 
+
+class TokenAdmin(admin.ModelAdmin):
+    def save_model(self,request,obj,form,change):
+        emailExist = ClinicManager.objects.filter(email=obj.email).count() + WarehousePersonnel.objects.filter(email=obj.email).count() + Dispatcher.objects.filter(email=obj.email).count() +  HospitalAuthority.objects.filter(email=obj.email).count()
+        if emailExist == 1:
+            messages.error(request, "Email already exists")
+            # return render() -> render back to admin page to ask for new email
+        else:
+            email = []
+            email.append(obj.email)
+            token = obj.token
+            content = 'Here is your link for registration http://127.0.0.1:8000/main/preregistration?token=' + str(token) +'\n'
+
+            send_mail('Token Registration',content,'navig8.comp3297@gmail.com',email,fail_silently=False,)
+            return super(TokenAdmin,self).save_model(request,obj,form,change)
+admin.site.register(Token,TokenAdmin)
+
+
 #unregister
 admin.site.unregister(User)
 admin.site.unregister(Group)
